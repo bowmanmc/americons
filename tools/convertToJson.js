f532c578dc9bc330ec81c8fec82d65cd90cc9eaf
Michael Bowman <bowmanmc@gmail.com>
2016-12-19 20:07:38 -0500

    convert to json high/med/low logic

--- tools/convertToJson.js
+++ tools/convertToJson.js
@@ -2,30 +2,35 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const shapefile = require('shapefile');
 
+const TIGER = require('../tiger.config.js');
 const IN_DIR = 'shp';
 const OUT_DIR = 'json';
 
-mkdirp.sync(OUT_DIR);
 
-let shpFile = `${IN_DIR}/cb_2015_us_state_500k.shp`;
-let outFile = `${OUT_DIR}/cb_2015_us_state_500k.json`;
-let out = fs.createWriteStream(outFile);
+TIGER.files.forEach(tigerFile => {
 
-shapefile.open(shpFile).then(source => {
-    out.write("{\"type\":\"FeatureCollection\",\"bbox\":");
-    out.write(JSON.stringify(source.bbox));
-    out.write(",\"features\":[");
-    return source.read().then(function(result) {
-        if (result.done) return;
-        out.write(JSON.stringify(result.value));
-        return source.read().then(function repeat(result) {
+    let shpFile = `${IN_DIR}/${tigerFile.resolution}/${tigerFile.filename}.shp`;
+    let outPath = `${OUT_DIR}/${tigerFile.resolution}`;
+    let outFile = `${outPath}/${tigerFile.filename}.json`;
+    mkdirp.sync(outPath);
+
+    let out = fs.createWriteStream(outFile);
+
+    shapefile.open(shpFile).then(source => {
+        out.write('{\"type\":\"FeatureCollection\",\"bbox\":');
+        out.write(JSON.stringify(source.bbox));
+        out.write(',\"features\":[');
+        return source.read().then(function(result) {
             if (result.done) return;
-            out.write(",");
             out.write(JSON.stringify(result.value));
-            return source.read().then(repeat);
+            return source.read().then(function repeat(result) {
+                if (result.done) return;
+                out.write(',');
+                out.write(JSON.stringify(result.value));
+                return source.read().then(repeat);
+            });
+        }).then(function() {
+            out.end(']}\n');
         });
-    }).then(function() {
-        //out[out === process.stdout ? "write" : "end"]("]}\n");
-        out.end("]}\n");
     });
 });
