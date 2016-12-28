69465e60cae5a01592609075d7245643b70a8e2b
Michael Bowman <bowmanmc@gmail.com>
2016-12-27 21:05:08 -0500

    sass for all the states

--- gulpfile.js
+++ gulpfile.js
@@ -9,21 +9,14 @@ const FONT_DIR = 'fonts';
 const RUN_TIME = Math.round(Date.now()/1000);
 
 
-function buildTask(resolution) {
+gulp.task('fonts', function() {
     mkdirp.sync(FONT_DIR);
 
-    let outFile = `${FONT_DIR}/americons-${resolution}.codes.json`;
-    let fontName = `${FONT_NAME}-${resolution}`;
+    let outFile = `${FONT_DIR}/americons.codes.json`;
 
-    if (!resolution) {
-        outFile = `${FONT_DIR}/americons.codes.json`;
-        fontName = FONT_NAME;
-        resolution = 'med'; // default resolution
-    }
-
-    return gulp.src([`svg/${resolution}/*.svg`])
+    return gulp.src([`svg/*.svg`])
         .pipe(iconfont({
-            fontName: fontName,
+            fontName: FONT_NAME,
             prependUnicode: false,
             formats: ['eot', 'svg', 'ttf', 'woff', 'woff2'],
             timestamp: RUN_TIME
@@ -35,7 +28,6 @@ function buildTask(resolution) {
                 let name = glyph.name;
                 let code = glyph.unicode[0].charCodeAt(0);
                 let hex = code.toString(16);
-                //console.log(`${name} - ${hex}`);
                 codes.push({
                     name: name,
                     code: code,
@@ -45,22 +37,4 @@ function buildTask(resolution) {
             fs.writeFileSync(outFile, JSON.stringify(codes));
         })
         .pipe(gulp.dest(FONT_DIR));
-}
-
-gulp.task('fonts', ['fonts-default']);
-
-gulp.task('fonts-default', function() {
-    return buildTask();
-});
-
-gulp.task('fonts-low', function() {
-    return buildTask('low');
-});
-
-gulp.task('fonts-med', function() {
-    return buildTask('med');
-});
-
-gulp.task('fonts-high', function() {
-    return buildTask('high');
 });
