e827a8357d46865dd9910b34de2f034db4a5b2f2
Michael Bowman <bowmanmc@gmail.com>
2016-12-28 17:48:23 -0500

    template for icons html

--- tools/applyTemplates.js
+++ tools/applyTemplates.js
@@ -1,19 +1,31 @@
 const ejs = require('ejs');
 const fs = require('fs');
 
-const TPL_ICONS = 'tools/_icons.scss.ejs';
 const CODES = JSON.parse(fs.readFileSync('fonts/americons.codes.json', 'utf8'));
-const OUT_PATH = 'sass/_icons.scss';
+
+const TPL_SCSS = 'tools/_icons.scss.ejs';
+const TPL_HTML = 'tools/icons.html.ejs';
+const OUT_SCSS = 'sass/_icons.scss';
+const OUT_HTML = 'www/_includes/icons.html';
 
 
 function generateScss() {
 
-    const template = fs.readFileSync(TPL_ICONS, 'utf8');
+    const template = fs.readFileSync(TPL_SCSS, 'utf8');
     const output = ejs.render(template, {
         icons: CODES
     });
-    fs.writeFileSync(OUT_PATH, output);
+    fs.writeFileSync(OUT_SCSS, output);
 }
 
-// generate sass
+function generateHtml() {
+    const template = fs.readFileSync(TPL_HTML, 'utf8');
+    const output = ejs.render(template, {
+        icons: CODES
+    });
+    fs.writeFileSync(OUT_HTML, output);
+}
+
+
 generateScss();
+generateHtml();
--- www/index.md
+++ www/index.md
@@ -6,3 +6,5 @@ permalink: /
 
 
 SVGs and Icon Font for the geographic boundaries of the United States
+
+{% include icons.html %}
